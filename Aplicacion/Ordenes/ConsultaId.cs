50ae63f4ee28df7519a2c42ab445e82b70d8c951
Carlos-Elias-Lopez <carlos.acuna8851@est.uca.edu.ni>
2021-11-26 10:51:39 -0600

    Registrar orden y detalleOrden

---
+++ Aplicacion/Ordenes/ConsultaId.cs
@@ -0,0 +1,32 @@
+﻿using Dominio.Model;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Ordenes
+{
+    public class ConsultaId
+    {
+        public class ConsultaUnica : IRequest<TblOrdenes>
+        {
+            public Guid Id { get; set; }
+        }
+        public class Manejador : IRequestHandler<ConsultaUnica, TblOrdenes>
+        {
+            private readonly netLisContext _context;
+            public Manejador(netLisContext context)
+            {
+                _context = context;
+            }
+            public async Task<TblOrdenes> Handle(ConsultaUnica request, CancellationToken cancellationToken)
+            {
+                var ordenes = await _context.TblOrdenes.FindAsync(request.Id);
+                return ordenes;
+            }
+        }
+    }
+}
